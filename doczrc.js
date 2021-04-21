6d82c71f3bcb193e1f2bf1093f77a758cd7cbbc1
Luka Hartwig <l.hartwig@mytaxi.com>
2021-04-21 16:45:57 +0200

    docs: removed some pages and internal references
    Removed SECURITY and MAINTAINERS page which were added
    accidentally, as well as a link to intenal slack.

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,15 @@
 export default {
     title: `Wave`,
     menu: ['Get Started', 'Releases', 'Contributing', 'Essentials', 'Components'],
-    ignore: ['CONTRIBUTING.md', 'RELEASING.md', 'README.md', 'fixtures/**/*.md', '.idea/**/*'],
+    ignore: [
+        'CONTRIBUTING.md',
+        'RELEASING.md',
+        'README.md',
+        'SECURITY.md',
+        'MAINTAINERS.md',
+        'fixtures/**/*.md',
+        '.idea/**/*'
+    ],
     propsParser: false,
     typescript: true,
     repository: false
