c8aee6d7a4804455f2d0b6fa0eda41e79c3a44ef
Leonardo Di Vittorio <leo@MacBook-Pro-de-Leonardo.local>
2021-05-05 11:29:34 +0200

    fix: isLastPage true if 0 items

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -105,7 +105,7 @@ const Pagination: React.FC<PaginationProps> = ({
     onSkipBackward
 }) => {
     const isFirstPage = value === 1;
-    const isLastPage = value === Math.ceil(totalItems / pageSize);
+    const isLastPage = value === Math.ceil(totalItems / pageSize) || !totalItems;
 
     return (
         <Container>
