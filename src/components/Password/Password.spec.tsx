958a6cb554c4371830586528ce8135b05c10d97d
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2021-05-18 15:15:24 +0200

    test: check all the elements of the <Password /> is in place

---
+++ src/components/Password/Password.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { Password } from './Password';
+
+describe('Password', () => {
+    it('is in hidden password mode and button to toggle', () => {
+        render(<Password label="password" />);
+
+        expect(screen.getByLabelText('password')).toHaveAttribute('type', 'password');
+        expect(screen.getByRole('button', { name: 'show' })).toBeInTheDocument();
+    });
+
+    describe('when toggle button is clicked', () => {
+        it.todo('when mode is hide password, changes the mode to show');
+        it.todo('when mode is show password, changes the mode to hide');
+    });
+});
