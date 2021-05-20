fd3729bc9d2af91cedcec2c47ba4c7d1abbb2e5e
Alexis Duran <alexis.duranm@gmail.com>
2021-05-20 12:54:25 +0200

    chore(icons): allow branded icons to be separated to avoid Color prop

---
+++ src/icons/brands/index.ts
@@ -0,0 +1,6 @@
+export { default as AmericanExpressIcon } from './AmericanExpressIcon';
+export { default as ApplePayIcon } from './ApplePayIcon';
+export { default as GooglePayIcon } from './GooglePayIcon';
+export { default as MastercardIcon } from './MastercardIcon';
+export { default as PayPalIcon } from './PayPalIcon';
+export { default as VisaIcon } from './VisaIcon';
