5cf02b97c1111b09a97d2f61a003f4185f4bf5ee
Alexis Duran <alexis.duranm@gmail.com>
2021-05-21 21:50:32 +0200

    fix: Allow to set icon color for payments icons (#76)
    * build(icons): avoid to exclude colors from payments
    
    while the code will work as expected (branded payments like MasterCard won't be affected by the
    color)the type definition is wrong as it will "allow color" for all icons
    
    * feat(icons): run npm generate to allow color on payments
    
    * docs(release): add release notes
    
    * chore(icons): allow branded icons to be separated to avoid Color prop

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,11 +1,12 @@
 export * from './basic';
 export * from './battery';
+export * from './brands';
+export * from './deprecated';
 export * from './mobility';
 export * from './options';
 export * from './payment';
 export * from './service-type';
 export * from './trip';
-export * from './deprecated';
 
 export { Flag } from './Flag';
 export { IconProps } from './IconProps';
