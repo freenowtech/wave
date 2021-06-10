4575990ad15b7da697bada492a324ac9dbdb48dc
Leonardo Di Vittorio <leo@MacBook-Pro-de-Leonardo.local>
2021-06-10 17:46:41 +0200

    refactor: enum position inFrom

--- src/components/Offcanvas/components/SideCard.tsx
+++ src/components/Offcanvas/components/SideCard.tsx
@@ -7,63 +7,65 @@ import { Card, CardProps } from '../../Card/Card';
 const ANIMATION_DURATION = 150;
 const TRANSITION_KEY = 'centered-card-animation';
 
-const fromLeft = css`
-    &.${TRANSITION_KEY}-appear, &.${TRANSITION_KEY}-enter {
-        opacity: 0;
-        transform: translate(calc(-50% - 1rem), 0%);
-    }
+const inFrom = {
+    right: css`
+        &.${TRANSITION_KEY}-appear, &.${TRANSITION_KEY}-enter {
+            opacity: 0;
+            transform: translate(calc(50% + 1rem), 0%);
+        }
 
-    &.${TRANSITION_KEY}-appear-active, &.${TRANSITION_KEY}-enter-active {
-        opacity: 1;
-        transform: translate(0%, 0%);
-    }
+        &.${TRANSITION_KEY}-appear-active, &.${TRANSITION_KEY}-enter-active {
+            opacity: 1;
+            transform: translate(0%, 0%);
+        }
 
-    &.${TRANSITION_KEY}-exit {
-        opacity: 1;
-        transform: translate(0%, 0%);
-    }
+        &.${TRANSITION_KEY}-exit {
+            opacity: 1;
+            transform: translate(0%, 0%);
+        }
 
-    &.${TRANSITION_KEY}-exit-done, &.${TRANSITION_KEY}-exit-active {
-        opacity: 0;
-        transform: translate(-10%, 0%);
-    }
-`;
+        &.${TRANSITION_KEY}-exit-done, &.${TRANSITION_KEY}-exit-active {
+            opacity: 0;
+            transform: translate(+10%, 0%);
+        }
+    `,
+    left: css`
+        &.${TRANSITION_KEY}-appear, &.${TRANSITION_KEY}-enter {
+            opacity: 0;
+            transform: translate(calc(-50% - 1rem), 0%);
+        }
 
-const fromRight = css`
-    &.${TRANSITION_KEY}-appear, &.${TRANSITION_KEY}-enter {
-        opacity: 0;
-        transform: translate(calc(50% + 1rem), 0%);
-    }
+        &.${TRANSITION_KEY}-appear-active, &.${TRANSITION_KEY}-enter-active {
+            opacity: 1;
+            transform: translate(0%, 0%);
+        }
 
-    &.${TRANSITION_KEY}-appear-active, &.${TRANSITION_KEY}-enter-active {
-        opacity: 1;
-        transform: translate(0%, 0%);
-    }
+        &.${TRANSITION_KEY}-exit {
+            opacity: 1;
+            transform: translate(0%, 0%);
+        }
 
-    &.${TRANSITION_KEY}-exit {
-        opacity: 1;
-        transform: translate(0%, 0%);
-    }
+        &.${TRANSITION_KEY}-exit-done, &.${TRANSITION_KEY}-exit-active {
+            opacity: 0;
+            transform: translate(-10%, 0%);
+        }
+    `
+};
 
-    &.${TRANSITION_KEY}-exit-done, &.${TRANSITION_KEY}-exit-active {
-        opacity: 0;
-        transform: translate(+10%, 0%);
-    }
-`;
-
-const right = css`
-    left: unset;
-    right: 0;
-    top: 0;
-    bottom: 0;
-`;
-
-const left = css`
-    left: 0;
-    right: unset;
-    top: 0;
-    bottom: 0;
-`;
+const position = {
+    right: css`
+        left: unset;
+        right: 0;
+        top: 0;
+        bottom: 0;
+    `,
+    left: css`
+        left: 0;
+        right: unset;
+        top: 0;
+        bottom: 0;
+    `
+};
 
 const StyledCard = styled(Card)<{ side?: string }>`
     position: fixed;
@@ -76,8 +78,8 @@ const StyledCard = styled(Card)<{ side?: string }>`
     will-change: transform, opacity;
     transition: transform ${ANIMATION_DURATION}ms ease-out, opacity ${ANIMATION_DURATION}ms ease-out;
 
-    ${p => (p.side === 'right' ? fromRight : fromLeft)};
-    ${p => (p.side === 'right' ? right : left)};
+    ${p => inFrom[p.side]};
+    ${p => position[p.side]};
 
     @media (max-width: calc(${p => p.width} + 2rem)) {
         width: calc(100% - 2rem);
