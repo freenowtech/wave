1f1b323ee0ff75070be61d8880c1484e38fde54a
Leonardo Di Vittorio <leo@MacBook-Pro-de-Leonardo.local>
2021-06-10 17:13:14 +0200

    docs: add examples for Offcanvas

---
+++ src/components/Offcanvas/docs/OffcanvasCreator.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Button, Headline, Text, TextButton } from '../..';
+import { Offcanvas } from '../Offcanvas';
+
+const offcanvasContent = dismiss => (
+    <>
+        <Headline as="h2">Add Note</Headline>
+        <Text as="p" weak my={3}>
+            Please keep in mind that comments are also read by other agents. Make sure to write comprehensible text.
+        </Text>
+        <Button onClick={dismiss}>Add Note</Button>
+        <TextButton ml={1} onClick={dismiss}>
+            Cancel
+        </TextButton>
+    </>
+);
+
+enum OffcanvasType {
+    NONE,
+    DEFAULT,
+    LEFT,
+    RIGHT,
+    NON_DISMISSIBLE
+}
+
+const OffcanvasCreator = () => {
+    const [offcanvas, setOffcanvas] = useState(OffcanvasType.NONE);
+
+    const openOffcanvas = (type: OffcanvasType) => () => {
+        setOffcanvas(type);
+    };
+
+    const hideOffcanvas = () => {
+        setOffcanvas(OffcanvasType.NONE);
+    };
+
+    return (
+        <>
+            <Button size="small" mr={1} onClick={openOffcanvas(OffcanvasType.DEFAULT)}>
+                Default (Left) Offcanvas
+            </Button>
+            <Button size="small" mr={1} onClick={openOffcanvas(OffcanvasType.RIGHT)}>
+                Right Offcanvas
+            </Button>
+            <Button size="small" onClick={openOffcanvas(OffcanvasType.NON_DISMISSIBLE)}>
+                Non-Dismissible Offcanvas
+            </Button>
+
+            {offcanvas == OffcanvasType.DEFAULT && (
+                <Offcanvas side="left" onClose={hideOffcanvas}>
+                    {offcanvasContent}
+                </Offcanvas>
+            )}
+            {offcanvas == OffcanvasType.RIGHT && (
+                <Offcanvas side="right" onClose={hideOffcanvas}>
+                    {offcanvasContent}
+                </Offcanvas>
+            )}
+            {offcanvas == OffcanvasType.NON_DISMISSIBLE && (
+                <Offcanvas onClose={hideOffcanvas} dismissible={false}>
+                    {offcanvasContent}
+                </Offcanvas>
+            )}
+        </>
+    );
+};
+
+export { OffcanvasCreator };
---
+++ src/components/Offcanvas/docs/OffcanvasPropsTable.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { PropsTable } from '../../../docs/PropsTable';
+
+export const OffcanvasPropsTable = () => {
+    const props = [
+        {
+            name: 'side',
+            type: '"left" | "right"',
+            description: 'Show the Offcanvas on the side of the page: left or right.',
+            defaultValue: '-'
+        },
+        {
+            name: 'dismissible',
+            type: 'boolean',
+            description: 'Makes the Offcanvas dismissible by the user.',
+            defaultValue: 'true'
+        },
+        {
+            name: 'onClose',
+            type: '() => void',
+            description:
+                'A function that will be called after the user has dismissed the Offcanvas and the Offcanvas has disappeared.',
+            defaultValue: '-'
+        }
+    ];
+    return <PropsTable props={props} />;
+};
