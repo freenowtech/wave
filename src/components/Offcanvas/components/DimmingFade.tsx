c937a485169bf051ee0b843725a5dee7dee43e94
Leonardo <leonardo.divittorio@live.it>
2021-06-11 14:18:21 +0200

    Feat: add offcanvas component (#115)
    * feat: add offcanvas component
    
    * test: add tests for Offcanvas
    
    * docs: add examples for Offcanvas
    
    * feat: add top bottom Offcanvas
    
    * refactor: enum position inFrom
    
    * feat: integrate top bottom Offcanvas
    
    * test: update with top bottom offcanvas
    
    * docs: add examples for top bottom offcanvas
    
    * fix: or syntax bug
    
    Co-authored-by: Leonardo Di Vittorio <leo@MacBook-Pro-de-Leonardo.local>

---
+++ src/components/Offcanvas/components/DimmingFade.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { CSSTransition } from 'react-transition-group';
+import styled, { css } from 'styled-components';
+import { Dimming } from '../../Dimming/Dimming';
+import { Colors } from '../../../essentials';
+
+const TRANSITION_KEY = 'dimming-fade-animation';
+const ANIMATION_DURATION = 200;
+
+const fadeInAnimation = (maxOpacity = 0.6) => css`
+    transition: opacity ${ANIMATION_DURATION}ms cubic-bezier(0.215, 0.61, 0.355, 1);
+
+    &.${TRANSITION_KEY}-appear, &.${TRANSITION_KEY}-enter {
+        opacity: 0;
+    }
+
+    &.${TRANSITION_KEY}-appear-active, &.${TRANSITION_KEY}-enter-active {
+        opacity: ${maxOpacity};
+    }
+
+    &.${TRANSITION_KEY}-exit {
+        opacity: ${maxOpacity};
+    }
+
+    &.${TRANSITION_KEY}-exit-done, &.${TRANSITION_KEY}-exit-active {
+        opacity: 0;
+    }
+`;
+
+const fullscreenDimming = css`
+    background-color: ${Colors.WHITE};
+    opacity: 1;
+`;
+
+const DimmingFadeStyled = styled(Dimming)<{ fullscreen?: boolean }>`
+    will-change: opacity;
+
+    ${p => (p.fullscreen ? fadeInAnimation(1) : fadeInAnimation())};
+    ${p => (p.fullscreen ? fullscreenDimming : null)}
+`;
+
+const DimmingFade: React.FC<{ visible: boolean; fullscreen?: boolean; onClick: () => void }> = ({
+    visible,
+    ...rest
+}) => {
+    return (
+        <CSSTransition in={visible} classNames={TRANSITION_KEY} timeout={ANIMATION_DURATION} unmountOnExit appear>
+            <DimmingFadeStyled {...rest} />
+        </CSSTransition>
+    );
+};
+
+export { DimmingFade, ANIMATION_DURATION };
