e806277b67639f29e2697b79c60a4754e283eed0
Jonah Möller <j.moeller@mytaxi.com>
2021-06-23 22:47:00 +0200

    feat: add semantic colors for poc

--- src/essentials/Colors/Colors.ts
+++ src/essentials/Colors/Colors.ts
@@ -30,3 +30,103 @@ export enum Colors {
     NEGATIVE_ORANGE_350 = '#FFC09E',
     NEGATIVE_ORANGE_50 = '#FFF6F1'
 }
+
+export const SemanticColors = {
+    button: {
+        primary: {
+            text: Colors.WHITE,
+            background: Colors.AUTHENTIC_BLUE_900,
+            borderColor: Colors.AUTHENTIC_BLUE_900,
+
+            hoverText: Colors.WHITE,
+            hoverBackground: Colors.AUTHENTIC_BLUE_1100,
+            hoverBorderColor: Colors.AUTHENTIC_BLUE_1100,
+
+            disabledText: Colors.WHITE,
+            disabledBackground: Colors.AUTHENTIC_BLUE_200,
+            disabledBorderColor: Colors.AUTHENTIC_BLUE_200,
+
+            textInverted: Colors.ACTION_BLUE_900,
+            backgroundInverted: Colors.WHITE,
+            borderColorInverted: Colors.WHITE,
+
+            hoverTextInverted: Colors.WHITE,
+            hoverBackgroundInverted: Colors.ACTION_BLUE_900,
+            hoverBorderColorInverted: Colors.ACTION_BLUE_900,
+
+            disabledTextInverted: Colors.AUTHENTIC_BLUE_350,
+            disabledBackgroundInverted: Colors.AUTHENTIC_BLUE_550,
+            disabledBorderColorInverted: Colors.AUTHENTIC_BLUE_550
+        },
+        secondary: {
+            text: Colors.AUTHENTIC_BLUE_900,
+            background: Colors.WHITE,
+            borderColor: Colors.AUTHENTIC_BLUE_200,
+
+            hoverText: Colors.AUTHENTIC_BLUE_900,
+            hoverBackground: Colors.AUTHENTIC_BLUE_50,
+            hoverBorderColor: Colors.AUTHENTIC_BLUE_200,
+
+            disabledText: Colors.AUTHENTIC_BLUE_200,
+            disabledBackground: Colors.WHITE,
+            disabledBorderColor: Colors.AUTHENTIC_BLUE_200,
+
+            textInverted: Colors.WHITE,
+            backgroundInverted: 'transparent',
+            borderColorInverted: Colors.WHITE,
+
+            hoverTextInverted: Colors.AUTHENTIC_BLUE_900,
+            hoverBackgroundInverted: Colors.WHITE,
+            hoverBorderColorInverted: Colors.WHITE,
+
+            disabledTextInverted: Colors.AUTHENTIC_BLUE_550,
+            disabledBackgroundInverted: 'transparent',
+            disabledBorderColorInverted: Colors.AUTHENTIC_BLUE_550
+        },
+        danger: {
+            text: Colors.WHITE,
+            background: Colors.NEGATIVE_ORANGE_900,
+            borderColor: Colors.NEGATIVE_ORANGE_900,
+
+            hoverText: Colors.WHITE,
+            hoverBackground: Colors.NEGATIVE_ORANGE_1000,
+            hoverBorderColor: Colors.NEGATIVE_ORANGE_1000,
+
+            disabledText: Colors.WHITE,
+            disabledBackground: Colors.AUTHENTIC_BLUE_200,
+            disabledBorderColor: Colors.AUTHENTIC_BLUE_200,
+
+            textInverted: Colors.WHITE,
+            backgroundInverted: Colors.NEGATIVE_ORANGE_900,
+            borderColorInverted: Colors.NEGATIVE_ORANGE_900,
+
+            hoverTextInverted: Colors.WHITE,
+            hoverBackgroundInverted: Colors.NEGATIVE_ORANGE_1000,
+            hoverBorderColorInverted: Colors.NEGATIVE_ORANGE_1000,
+
+            disabledTextInverted: Colors.WHITE,
+            disabledBackgroundInverted: Colors.AUTHENTIC_BLUE_200,
+            disabledBorderColorInverted: Colors.AUTHENTIC_BLUE_200
+        }
+    },
+    textButton: {
+        primary: {
+            text: Colors.ACTION_BLUE_900,
+            hoverText: Colors.ACTION_BLUE_1000,
+            disabledText: Colors.AUTHENTIC_BLUE_200,
+
+            textInverted: Colors.WHITE,
+            hoverTextInverted: Colors.AUTHENTIC_BLUE_350,
+            disabledTextInverted: Colors.AUTHENTIC_BLUE_550
+        },
+        danger: {
+            text: Colors.NEGATIVE_ORANGE_900,
+            hoverText: Colors.NEGATIVE_ORANGE_1000,
+            disabledText: Colors.AUTHENTIC_BLUE_200,
+
+            textInverted: Colors.NEGATIVE_ORANGE_900,
+            hoverTextInverted: Colors.NEGATIVE_ORANGE_1000,
+            disabledTextInverted: Colors.AUTHENTIC_BLUE_550
+        }
+    }
+};
