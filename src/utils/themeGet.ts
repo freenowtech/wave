38703e0889189f45b2da59c8098dd3e3e5999b36
Jonah Möller <j.moeller@mytaxi.com>
2021-06-23 22:46:09 +0200

    feat: add warning when fetching a theme property that is not defined

--- src/utils/themeGet.ts
+++ src/utils/themeGet.ts
@@ -2,4 +2,10 @@ import themeGet from '@styled-system/theme-get';
 import { get as getKey } from 'styled-system';
 import { theme } from '../essentials/theme';
 
-export const get = key => themeGet(key, getKey(theme, key));
+export const get = key => {
+    if (getKey(theme, key) === undefined) {
+        console.warn(`Theme key "${key}" not found in theme.`);
+    }
+
+    return themeGet(key, getKey(theme, key));
+};
