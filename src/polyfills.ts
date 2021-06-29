f342db77665d78523a091dd05210284d87b3f0e2
Jonah Möller <j.moeller@mytaxi.com>
2021-06-29 16:00:32 +0200

    docs: add library that requires the polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,7 +1,7 @@
 import { isSSR } from './utils/isSSR';
 
 if (!isSSR()) {
-    // create an alias for the crypto api for IE11
+    // create an alias for the crypto api for IE11 (required by `nanoid`)
     if (!window.crypto) {
         // @ts-ignore
         window.crypto = window.msCrypto;
