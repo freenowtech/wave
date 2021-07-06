31b14158a28e4f2e14c4c0d82c0acf17337af724
Jonah Möller <jonah.moeller@googlemail.com>
2021-07-06 22:58:25 +0200

    docs: run prettier

--- src/essentials/Colors/docs/SemanticColorsTable.tsx
+++ src/essentials/Colors/docs/SemanticColorsTable.tsx
@@ -6,10 +6,10 @@ import { Colors, SemanticColors } from '../Colors';
 function flattenObj(obj: {}, parent?: {}, res: {} = {}): {} {
     for (const key in obj) {
         const propName = parent ? parent + '.' + key : key;
-        if (typeof obj[ key ] == 'object') {
-            flattenObj(obj[ key ], propName, res);
+        if (typeof obj[key] == 'object') {
+            flattenObj(obj[key], propName, res);
         } else {
-            res[ propName ] = obj[ key ];
+            res[propName] = obj[key];
         }
     }
 
@@ -18,7 +18,7 @@ function flattenObj(obj: {}, parent?: {}, res: {} = {}): {} {
 
 const ColorBlock = styled.div<{ color: string }>`
     background-color: ${p => p.color};
-    border: 0.0625rem solid ${p => p.color === Colors.WHITE ? Colors.AUTHENTIC_BLUE_200 : p.color};
+    border: 0.0625rem solid ${p => (p.color === Colors.WHITE ? Colors.AUTHENTIC_BLUE_200 : p.color)};
     height: 1rem;
     width: 4rem;
 `;
@@ -29,20 +29,26 @@ export const SemanticColorsTable = () => {
     return (
         <Table rowStyle="lines" width="100%">
             <thead>
-            <TableRow>
-                <TableHeaderCell>Color</TableHeaderCell>
-                <TableHeaderCell>Name</TableHeaderCell>
-                <TableHeaderCell>Hex Code</TableHeaderCell>
-            </TableRow>
+                <TableRow>
+                    <TableHeaderCell>Color</TableHeaderCell>
+                    <TableHeaderCell>Name</TableHeaderCell>
+                    <TableHeaderCell>Hex Code</TableHeaderCell>
+                </TableRow>
             </thead>
             <tbody>
-            {Object.keys(flatSemanticColors).map(key => (
-                <TableRow key={key}>
-                    <TableCell><ColorBlock color={flatSemanticColors[ key ]} /></TableCell>
-                    <TableCell><code>{key}</code></TableCell>
-                    <TableCell><code>{flatSemanticColors[ key ]}</code></TableCell>
-                </TableRow>
-            ))}
+                {Object.keys(flatSemanticColors).map(key => (
+                    <TableRow key={key}>
+                        <TableCell>
+                            <ColorBlock color={flatSemanticColors[key]} />
+                        </TableCell>
+                        <TableCell>
+                            <code>{key}</code>
+                        </TableCell>
+                        <TableCell>
+                            <code>{flatSemanticColors[key]}</code>
+                        </TableCell>
+                    </TableRow>
+                ))}
             </tbody>
         </Table>
     );
