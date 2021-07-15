af5980819168c7fde433e1d929ecf58c5065aaf4
Jonah Möller <snapsnapturtle@users.noreply.github.com>
2021-07-15 11:09:23 +0200

    chore(fixtures): Update test fixture dependencies (webpack) (#142)
    * chore: update fixture dependencies for babel build
    
    * chore: update fixture dependencies for typescript build

--- fixtures/babel-build/webpack.config.js
+++ fixtures/babel-build/webpack.config.js
@@ -2,11 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+    mode: 'production',
     module: {
         rules: [
             {
