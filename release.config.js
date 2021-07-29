3fe11f1ae8c5cc455286389f5a5e20761d8c74ce
Luka Hartwig <l.hartwig@mytaxi.com>
2021-07-29 11:47:06 +0200

    ci: add prerelease flag for next branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ['main', 'next'],
+    branches: ['main', { name: 'next', prerelease: true }],
     plugins: [
         [
             '@semantic-release/commit-analyzer',
