85b55d097fa5f68634779998a1f2a4e9f2994959
Lloyd Francis <lloydaf@gmail.com>
2021-09-07 15:11:56 +0200

    improvement: refining the svg to reflect the design specs

--- src/components/Checkbox/components/Checkmark.tsx
+++ src/components/Checkbox/components/Checkmark.tsx
@@ -58,7 +58,7 @@ const Checkmark = styled.input<CheckmarkProps>`
     }
 
     &:indeterminate {
-        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 20 20' data-reactroot=''%3E%3Cpath d='M15.833 10.833V9.167H4.167v1.666z' fill='%23FFF' fill-rule='nonzero'%3E%3C/path%3E%3C/svg%3E");
+        background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='10' height='2' x='3' y='7' rx='1' fill='%23FFF' fill-rule='nonzero'/%3E%3C/svg%3E%0A");
         background-color: ${props => (props.error ? Colors.NEGATIVE_ORANGE_900 : Colors.ACTION_BLUE_900)};
         box-shadow: inset 0 0 0 0.125rem ${props => (props.error ? Colors.NEGATIVE_ORANGE_900 : Colors.ACTION_BLUE_900)};
     }
