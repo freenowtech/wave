1d50fb53a492854f0e10dab5f7c44678570f47f6
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2021-11-25 17:55:00 +0100

    test(text): update tests to use semantic colors
    Check the rendered colors of the `Text` component matches the colors from the semantic colors scheme

--- src/components/Text/Text.spec.tsx
+++ src/components/Text/Text.spec.tsx
@@ -1,31 +1,46 @@
 import { render } from '@testing-library/react';
 import * as React from 'react';
-import { Colors } from '../../essentials';
+import { SemanticColors } from '../../essentials/Colors/Colors';
+
 import { Text } from './Text';
 
 describe('Text', () => {
-    it('renders a <span> by default', () => {
-        expect(render(<Text />)).toMatchHtmlTag('span');
+    describe('default render', () => {
+        it('renders a <span> by default', () => {
+            expect(render(<Text />)).toMatchHtmlTag('span');
+        });
+
+        it('has a primary color by default', () => {
+            expect(render(<Text />).container.firstChild).toHaveStyle(`
+            color: ${SemanticColors.text.primary};
+        `);
+        });
+
+        it('renders the children', () => {
+            expect(render(<Text>Content</Text>).getByText('Content')).toBeInTheDocument();
+        });
     });
 
     it('respects the "as" prop', () => {
         expect(render(<Text as="b" />)).toMatchHtmlTag('b');
     });
 
-    it('renders the children', () => {
-        expect(render(<Text>Content</Text>).getByText('Content')).toBeInTheDocument();
+    it('has an inverted primary color if inverted property is set', () => {
+        expect(render(<Text inverted />).container.firstChild).toHaveStyle(`
+            color: ${SemanticColors.text.primaryInverted};
+        `);
     });
 
     describe('if the weak property is set', () => {
         it('has a dim color', () => {
             expect(render(<Text weak />).container.firstChild).toHaveStyle(`
-                color: ${Colors.AUTHENTIC_BLUE_550};
+                color: ${SemanticColors.text.secondary};
             `);
         });
 
         it('has a dimmer color if inverted', () => {
             expect(render(<Text weak inverted />).container.firstChild).toHaveStyle(`
-                color: ${Colors.AUTHENTIC_BLUE_350};
+                color: ${SemanticColors.text.secondaryInverted};
             `);
         });
     });
@@ -33,13 +48,13 @@ describe('Text', () => {
     describe('if the `secondary` property is set', () => {
         it('has a dim color', () => {
             expect(render(<Text secondary />).container.firstChild).toHaveStyle(`
-                color: ${Colors.AUTHENTIC_BLUE_550};
+                color: ${SemanticColors.text.secondary};
             `);
         });
 
         it('has a dimmer color if inverted', () => {
             expect(render(<Text secondary inverted />).container.firstChild).toHaveStyle(`
-                color: ${Colors.AUTHENTIC_BLUE_350};
+                color: ${SemanticColors.text.secondaryInverted};
             `);
         });
     });
@@ -47,19 +62,13 @@ describe('Text', () => {
     describe('if the disabled property is set', () => {
         it('has the disabled color', () => {
             expect(render(<Text disabled />).container.firstChild).toHaveStyle(`
-                color: ${Colors.AUTHENTIC_BLUE_200};
+                color: ${SemanticColors.text.disabled};
             `);
         });
         it('has a stronger disabled color if inverted', () => {
             expect(render(<Text disabled inverted />).container.firstChild).toHaveStyle(`
-                color: ${Colors.AUTHENTIC_BLUE_550};
+                color: ${SemanticColors.text.disabledInverted};
             `);
         });
-    });
-
-    it('has a bright color if inverted property is set', () => {
-        expect(render(<Text inverted />).container.firstChild).toHaveStyle(`
-            color: ${Colors.WHITE};
-        `);
     });
 });
