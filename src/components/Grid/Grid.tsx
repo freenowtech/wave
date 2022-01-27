584f53f058152b99e6a1a8fba1d929de0a02ce37
Jonathan Trujillo <313951+jonotrujillo@users.noreply.github.com>
2022-01-27 16:23:33 +0100

    refactor: rework flex approach

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -3,16 +3,21 @@ import styled from 'styled-components';
 import { Spaces } from '../..';
 import { Box, BoxProps } from '../Box/Box';
 
-const COLUMN_WIDTH = 100 / 12;
+const COLUMN_WIDTH_RATIO = 100 / 12;
 const GAP = Spaces[3];
-
-const Row = styled(Box).attrs({ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' })`
-    gap: ${GAP};
-    margin-right: -${GAP};
-`;
 
 type ColumnOffset = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
 type ColumnSpan = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+const BorderBoxWrapper = styled(Box)`
+    box-sizing: border-box;
+`;
+
+type RowProps = BoxProps;
+
+const Row: FC<RowProps> = (props: RowProps) => (
+    <Box display="flex" flexWrap="wrap" marginRight={`-${GAP}`} {...props} />
+);
 
 interface ColumnProps extends BoxProps {
     span?: ColumnSpan;
@@ -20,10 +25,10 @@ interface ColumnProps extends BoxProps {
 }
 
 const Column: FC<ColumnProps> = ({ span = 1, offset = 0, ...restProps }: ColumnProps) => {
-    const marginLeft = `${offset * COLUMN_WIDTH}%`;
-    const width = `calc(${span * COLUMN_WIDTH}% - ${GAP})`;
+    const marginLeft = `${offset * COLUMN_WIDTH_RATIO}%`;
+    const width = `${span * COLUMN_WIDTH_RATIO}%`;
 
-    return <Box flex={`0 0 ${width}`} maxWidth={width} marginLeft={marginLeft} {...restProps} />;
+    return <BorderBoxWrapper flex={`0 0 ${width}`} marginLeft={marginLeft} paddingRight={GAP} {...restProps} />;
 };
 
-export { Row, Column, ColumnProps };
+export { Row, RowProps, Column, ColumnProps };
