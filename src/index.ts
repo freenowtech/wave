5d8da7837d8566e6c409461b2832e61c66357e65
Leonardo Di Vittorio <leonardo.divittorio@Leonardos-MacBook-Pro.local>
2022-04-29 11:24:59 +0200

    feat: export all hooks

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import './polyfills';
 export * from './components';
 export * from './essentials';
 export * from './icons';
+export * from './utils/hooks';
 export { get as themeGet } from './utils/themeGet';
---
+++ src/utils/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useControlledState } from './useControlledState';
+export { useGeneratedId } from './useGeneratedId';
+export { useIsEscKeyPressed } from './useIsEscKeyPressed';
+export { useToggle } from './useToggle';
