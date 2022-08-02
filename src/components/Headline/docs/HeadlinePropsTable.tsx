ddae60c1918c30f484042b81e85b21f64bb476b1
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2022-08-02 14:25:18 +0200

    docs(headline): update props table

--- src/components/Headline/docs/HeadlinePropsTable.tsx
+++ src/components/Headline/docs/HeadlinePropsTable.tsx
@@ -6,12 +6,18 @@ export const HeadlinePropsTable: FC = () => {
         {
             name: 'as',
             type: '"h1" | "h2" | "h3" | "h4" | "h5" | "h6"',
-            defaultValue: 'h1'
+            defaultValue: 'h1',
+            description: 'HTML-tag to use'
         },
         {
             name: 'size',
             type: '"xxl" | "xl" | "l" | "m" | "s" | "xs"',
-            description: `if "size" prop is not set , the size is determined based on "as" prop`
+            description: 'if "size" prop is not set , the size is determined based on "as" prop'
+        },
+        {
+            name: 'inverted',
+            type: 'boolean',
+            description: 'if "true" - sets bright color to the font'
         }
     ];
 
