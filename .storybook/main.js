19f1fc769c3a072edc2e8c5d2ea610fae2bec713
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2023-03-22 15:59:11 +0100

    docs: migrate divider page

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-    stories: ['../src/stories/**/*.stories.@(ts|tsx)'],
+    stories: ['../src/stories/**/*.stories.mdx', '../src/stories/**/*.stories.@(ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
     framework: '@storybook/react'
 };
