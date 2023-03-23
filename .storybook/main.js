023039a7d643b18cd6c892fc5cc61a327cec3444
Cristian <jazhiel.st@gmail.com>
2023-03-23 15:54:45 +0100

    WECH-23: Migrate components to storybook part 2 (#314)
    * Checkbox
    * Datepicker
    * Dimmer
    * Divider
    * Filepicker
    ---------
    
    Co-authored-by: Zack Jones <zack.jones@free-now.com>
    Co-authored-by: Cristian Sanchez <cristiansanchez@MacBook-Pro-von-Cristian.local>
    Co-authored-by: Nikolai Lopin <nikolai.lopin@mytaxi.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-    stories: ['../src/stories/**/*.stories.@(ts|tsx)'],
+    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
     framework: '@storybook/react'
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     viewMode: 'docs',
     controls: {
+        expanded: true,
         matchers: {
             color: /(background|color)$/i,
             date: /Date$/
