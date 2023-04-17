35b400a3ea0a3f44ea15cd4d5672f05e275dc23e
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2023-04-17 14:17:28 +0200

    chore: fix failing storybook
    Update typescript to the lowest supported version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,8 @@
 import { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-        '@storybook/addon-mdx-gfm'
-    ],
+    stories: ['../src/**/*.stories.mdx'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
     framework: {
         name: '@storybook/react-webpack5',
         options: {}
