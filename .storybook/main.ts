ae9425ab1e3e6a41a91f0177e7967c4abe926e3f
vknibbe <49642287+vknibbe@users.noreply.github.com>
2023-04-19 14:49:52 +0200

    docs: migrate essentials to storybook (#322)
    * essentials pages
    * Logo
    * Icons
    * remove IE 11 from the list of supported
    
    ---------
    
    Co-authored-by: lena <lena.rashkovan@free-now.com>
    Co-authored-by: Vanessa Knibbe <vanessa.knibbe@free-now.com>
    Co-authored-by: Nikolai Lopin <nikolai.lopin@mytaxi.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,17 @@
 import { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.(stories|storybook).mdx', '../src/**/*.stories.@(ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+    stories: [
+        '../docs/**/*.(stories|storybook).mdx',
+        '../src/**/*.(stories|storybook).mdx',
+        '../src/**/*.stories.@(ts|tsx)'
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-viewport'
+    ],
     framework: {
         name: '@storybook/react-webpack5',
         options: {}
---
+++ src/icons/docs/Flags.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react';
+import * as FlagIcons from '../flags';
+import { LargeFlag } from './LargeFlag';
+
+const meta: Meta = {
+    title: 'Essentials/Icons/Flags',
+    component: LargeFlag
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LargeFlag>;
+
+export const Primary: Story = {
+    name: 'Flags',
+    args: {
+        code: 'US'
+    },
+    argTypes: {
+        code: {
+            description: 'Two letters ISO country code.',
+            control: 'select',
+            options: Object.entries(FlagIcons).map(it => it[0])
+        }
+    }
+};
