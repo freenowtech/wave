34db0557f59365047101ba5aee074cf635bf7b28
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2023-04-24 22:50:17 +0200

    docs: moves Banner stories to the banner folder

---
+++ src/components/Banner/docs/Banner.stories.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { Banner } from '../Banner';
+import { Text } from '../../Text/Text';
+import { Button } from '../../Button/Button';
+
+const meta: Meta = {
+    title: 'Components/Banner',
+    component: Banner,
+    argTypes: {
+        children: {
+            description:
+                'Content of the banner. Can be `ReactNode` or a render function that receives a dismiss method',
+            control: { type: 'none' }
+        },
+        position: {
+            table: {
+                defaultValue: {
+                    summary: 'Depends on the variant prop value',
+                    detail: `
+                        info → top\nsuccess → top\ndanger → bottom
+                    `
+                }
+            }
+        }
+    },
+    args: {
+        children: (
+            <Text inverted>
+                In order to continuously improve our websites, and show offers and advertisements that are suited to
+                you, we use cookies, tracking and (re-) targeting technologies. Please see our Cookie policy for more
+                information. Tracking and (re-) targeting technologies will only be used if you click on Agree.
+            </Text>
+        )
+    },
+    decorators: [
+        Story => (
+            <div
+                style={{
+                    willChange: 'transform',
+                    height: '250px',
+                    border: '1px dashed gray',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Text secondary fontSize={3}>
+                    Page
+                </Text>
+                <Story />
+            </div>
+        )
+    ]
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Banner>;
+
+export const Default: Story = {
+    args: {
+        children: <Text inverted>Hello</Text>
+    }
+};
+
+export const Success: Story = {
+    args: {
+        variant: 'success'
+    }
+};
+
+export const Danger: Story = {
+    args: {
+        variant: 'danger',
+        children: <Text inverted>Oops! Something went wrong</Text>
+    }
+};
+
+export const PositionTop: Story = {
+    args: {
+        position: 'top'
+    }
+};
+
+export const WithDismissedFn: Story = {
+    args: {
+        children: dismiss => (
+            <>
+                <Text inverted>If you are tired of this banner, hit the button 👉 </Text>
+                <Button onClick={dismiss}>Close</Button>
+            </>
+        )
+    }
+};
