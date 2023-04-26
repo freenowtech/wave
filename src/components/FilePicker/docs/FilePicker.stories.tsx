eeec04e386fad1726c0af7f616b5813030b0d43b
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2023-04-26 13:30:14 +0200

    docs: migrate FilePicker docs to csf3

---
+++ src/components/FilePicker/docs/FilePicker.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { FilePicker } from '../FilePicker';
+import { Box } from '../../Box/Box';
+
+const meta: Meta = {
+    title: 'Form Elements/FilePicker',
+    component: FilePicker,
+    argTypes: {
+        capture: {
+            options: ['user', 'environment'],
+            control: 'select'
+        }
+    },
+    args: {
+        name: 'avatar',
+        label: 'Picture of you',
+        buttonText: 'Browse'
+    }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FilePicker>;
+
+export const Default: Story = {};
+
+export const WithBrowserButtonAlwaysVisible: Story = {
+    args: {
+        alwaysShowActionButton: true
+    }
+};
+
+export const WithMultilineLabel: Story = {
+    args: {
+        label: 'A very long label which can stretch to more than one line without any issues'
+    },
+    decorators: [
+        Story => (
+            <Box width="20rem">
+                <Story />
+            </Box>
+        )
+    ]
+};
+
+export const AcceptingOnlyImages: Story = {
+    args: {
+        accept: 'image/*'
+    }
+};
