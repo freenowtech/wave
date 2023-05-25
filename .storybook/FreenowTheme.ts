af082b04ad16b08b174644200590bbb76844d766
Jan Hamara <hamara.jan18@gmail.com>
2023-05-25 15:10:49 +0200

    docs: Add Storybook documentation landing page (#334)
    * feat(landing): add landing page for Storybook
    
    * docs(landing): finish the landing page integration
    
    * docs(theme): theme storybook with our branding
    
    * refactor(landingpage): mr review modifs part 1
    
    * refactor(landing): wrap landing page mdx in unstyled component
    
    ---------
    
    Co-authored-by: Ján Hamara <jan.hamara@free-now.com>

---
+++ .storybook/FreenowTheme.ts
@@ -0,0 +1,41 @@
+// .storybook/FreenowTheme.ts
+import { create } from '@storybook/theming/create';
+import logo from '../public/assets/freenow-logo.svg';
+
+export default create({
+    base: 'light',
+
+    // Typography
+    fontBase: '"YOURNOWSans-Medium", "Open Sans", sans-serif',
+    fontCode: 'monospace',
+
+    brandTitle: 'FREENOW',
+    brandUrl: 'https://wave.free-now.com/',
+    brandImage: logo,
+    brandTarget: '_self',
+
+    //
+    colorPrimary: '#CA0928',
+    colorSecondary: '#585C6D',
+
+    // UI
+    appBg: '#ffffff',
+    appContentBg: '#ffffff',
+    appBorderColor: '#585C6D',
+    appBorderRadius: 4,
+
+    // Text colors
+    textColor: '#10162F',
+    textInverseColor: '#ffffff',
+
+    // Toolbar default and active colors
+    barTextColor: '#9E9E9E',
+    barSelectedColor: '#585C6D',
+    barBg: '#ffffff',
+
+    // Form colors
+    inputBg: '#ffffff',
+    inputBorder: '#10162F',
+    inputTextColor: '#10162F',
+    inputBorderRadius: 2
+});
