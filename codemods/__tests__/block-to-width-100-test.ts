99e23eb076014135e132dbec96be7e07e0c4957d
Martí Malek <marti.malek@free-now.com>
2023-08-25 16:40:02 +0200

    chore: add single quote config option to block codemod

--- codemods/__tests__/block-to-width-100-test.ts
+++ codemods/__tests__/block-to-width-100-test.ts
@@ -1,4 +1,4 @@
 jest.autoMockOff();
 const { defineTest } = require('jscodeshift/dist/testUtils');
 
-defineTest(__dirname, 'block-to-width-100', null, 'block-to-width-100', { parser: 'tsx' });
+defineTest(__dirname, 'block-to-width-100', { quote: 'single' }, 'block-to-width-100', { parser: 'tsx' });
