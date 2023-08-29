24e4f2b155df410f0325c3c97bb332f49657ddae
Martí Malek <marti.malek@free-now.com>
2023-08-29 12:44:11 +0200

    feat: pass print options to block codemod

--- src/codemods/__testfixtures__/colors-to-css-vars/color-in-JSX-multi-import.output.tsx
+++ src/codemods/__testfixtures__/colors-to-css-vars/color-in-JSX-multi-import.output.tsx
@@ -1,5 +1,5 @@
 import { CloseIcon } from '@freenow/wave';
 
 export const CloseIconWrapper = () => (
-    <CloseIcon data-testid="dismissBanner" color="var(--wave-b-color-white)" cursor="pointer" size={18} />
+    <CloseIcon data-testid="dismissBanner" color={'var(--wave-b-color-white)'} cursor="pointer" size={18} />
 );
