2a818c34fbd0ba68221f39224a939fa2175314b4
Martí Malek <marti.malek@free-now.com>
2023-09-19 13:10:23 +0200

    test: fix codemods tests styling

--- src/codemods/__testfixtures__/block-to-width-100.output.tsx
+++ src/codemods/__testfixtures__/block-to-width-100.output.tsx
@@ -2,7 +2,7 @@ import { Button } from '@freenow/wave';
 import React from 'react';
 
 export const ButtonTest = (): JSX.Element => (
-    <Button width="100%" mr="1" mt="3" disabled>
+    <Button width='100%' mr="1" mt="3" disabled>
         Clone
     </Button>
 );
--- src/codemods/__testfixtures__/weak-to-secondary/boolean-false.output.tsx
+++ src/codemods/__testfixtures__/weak-to-secondary/boolean-false.output.tsx
@@ -1,3 +1,7 @@
 import { Text } from '@freenow/wave';
 
-export const TextTest = () => <Text fontSize={1}>Just a text</Text>;
+export const TextTest = () => (
+    <Text fontSize={1}>
+        Just a text
+    </Text>
+);
