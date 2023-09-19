c5220c2f68a1a98b38386ef0225662f1e537f49a
Martí Malek <marti.malek@free-now.com>
2023-09-19 17:36:57 +0200

    feat: add codemod for placement prop value replacement in tooltip

---
+++ src/codemods/__tests__/tooltip-placement-test.ts
@@ -0,0 +1,12 @@
+jest.autoMockOff();
+const { defineTest } = require('jscodeshift/dist/testUtils');
+
+const tests = ['basic-usage', 'no-placement', 'multi-usage', 'local-rename'];
+
+describe('tooltip-placement', () => {
+    tests.forEach(test =>
+        defineTest(__dirname, 'tooltip-placement', { quote: 'single' }, `tooltip-placement/${test}`, {
+            parser: 'tsx'
+        })
+    );
+});
