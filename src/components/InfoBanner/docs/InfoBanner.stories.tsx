9c089ec4c1f731d0e760854453de18fb39db1f4c
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2023-09-29 16:20:24 +0200

    docs(infobanner): revert docs

--- src/components/InfoBanner/docs/InfoBanner.stories.tsx
+++ src/components/InfoBanner/docs/InfoBanner.stories.tsx
@@ -1,10 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
-import React from 'react';
-import { InfoBannerCard } from '../blocks/InfoBannerCard';
+import { InfoBanner } from '../InfoBanner';
 
 const meta: Meta = {
     title: 'Components/InfoBanner',
-    component: InfoBannerCard,
+    component: InfoBanner,
     args: {
         title: 'Please do not share details about this information.',
         description: 'In case of uncertainty please contact Fraud department.'
@@ -13,36 +12,18 @@ const meta: Meta = {
 
 export default meta;
 
-type Story = StoryObj<typeof InfoBannerCard>;
+type Story = StoryObj<typeof InfoBanner>;
 
-export const Default: Story = {
-    args: {
-        children: (
-            <>
-                <InfoBannerCard.Title>document.status.documents.title.no.vehicle</InfoBannerCard.Title>
-
-                <InfoBannerCard.Description>
-                    document.status.documents.body.no.vehicle
-                </InfoBannerCard.Description>
-
-                <InfoBannerCard.Link>
-                    vehicles.addVehicle.button
-                </InfoBannerCard.Link>
-            </>
-        )
-    }
-};
+export const Default: Story = {};
 
 export const Success: Story = {
     args: {
-        ...Default.args,
         variant: 'success'
     }
 };
 
 export const Warning: Story = {
     args: {
-        ...Default.args,
         variant: 'warning',
         title: 'Possible Fraud',
         description: 'This passenger has more than 20 linked accounts.'
@@ -51,21 +32,18 @@ export const Warning: Story = {
 
 export const Error: Story = {
     args: {
-        ...Default.args,
         variant: 'error'
     }
 };
 
 export const Emphasized: Story = {
     args: {
-        ...Default.args,
         emphasized: true
     }
 };
 
 export const WithLink: Story = {
     args: {
-        ...Default.args,
         linkText: 'More info',
         linkUrl: 'https://free-now.com/'
     }
