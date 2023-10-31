d1ecead456ac351449e77a340aa3c44edad846ae
Martí Malek <marti.malek@free-now.com>
2023-10-31 16:27:09 +0100

    refactor: rename inverted codemod to be more generic

--- src/codemods/__tests__/inverted-to-wrapper-test.ts
+++ src/codemods/__tests__/inverted-to-wrapper-test.ts
@@ -3,9 +3,9 @@ const { defineTest } = require('jscodeshift/dist/testUtils');
 
 const tests = ['local-rename', 'explicit-boolean-false', 'explicit-boolean-true', 'styled-rename'];
 
-describe('inverted-to-flip-theme', () => {
+describe('inverted-to-wrapper', () => {
     tests.forEach(test =>
-        defineTest(__dirname, 'inverted-to-flip-theme', { quote: 'single' }, `inverted-to-flip-theme/${test}`, {
+        defineTest(__dirname, 'inverted-to-wrapper', { quote: 'single' }, `inverted-to-wrapper/${test}`, {
             parser: 'tsx'
         })
     );
