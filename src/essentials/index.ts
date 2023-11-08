a955714588c72fb9b0f278d0a6ec9ecf7b4dbf9e
Martí Malek <marti.malek@free-now.com>
2023-11-08 17:54:10 +0100

    chore: export ReadCssColorVariable from /essentials

--- src/essentials/index.ts
+++ src/essentials/index.ts
@@ -1,3 +1,4 @@
 export { Elevation } from './Elevation/Elevation';
 export { Spaces } from './Spaces/Spaces';
 export { Breakpoints, MediaQueries } from './Breakpoints/Breakpoints';
+export type { ReadCssColorVariable } from './Colors/types';
