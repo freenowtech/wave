16b55826f6934e7696cc7af790f4f107a5170d4e
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2023-11-09 15:08:17 +0100

    fix: expose color themes
    Signed-off-by: Nikolai Lopin <nikolai.lopin@mytaxi.com>

--- src/essentials/index.ts
+++ src/essentials/index.ts
@@ -1,3 +1,4 @@
 export { Elevation } from './Elevation/Elevation';
 export { Spaces } from './Spaces/Spaces';
 export { Breakpoints, MediaQueries } from './Breakpoints/Breakpoints';
+export { ClassicColors, ModernColors } from './Colors/globalStyles';
