ef3145cfe0a23dd2fc44139aaa2b3b997ac86574
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2024-01-05 16:35:50 +0100

    refactor: rename schema file to match the schema name
    Signed-off-by: Nikolai Lopin <nikolai.lopin@mytaxi.com>

--- src/essentials/index.ts
+++ src/essentials/index.ts
@@ -2,4 +2,4 @@ export { Elevation } from './Elevation/Elevation';
 export { Spaces } from './Spaces/Spaces';
 export { Breakpoints, MediaQueries } from './Breakpoints/Breakpoints';
 export { GlobalStyle as ClassicColors } from './Colors/Colors';
-export { GlobalStyle as ModernColors } from './Colors/RedesignedColors';
+export { GlobalStyle as ModernColors } from './Colors/ModernColors';
