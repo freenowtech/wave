8734552539c6517c617c7cc66059407eb1d9fbd6
Martí Malek <marti.malek@free-now.com>
2024-07-05 11:37:28 +0200

    feat: add util to enforce a color scheme on an html element

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './components';
 export * from './essentials';
-export * from './icons';
 export * from './hooks';
-export { get as themeGet } from './utils/themeGet';
-export { getSemanticValue } from './utils/cssVariables';
+export * from './icons';
+export * from './utils';
