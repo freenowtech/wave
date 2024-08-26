6dd56327456a347764a2d6822ad1c53d8c670e69
Martí Malek <marti.malek@free-now.com>
2024-08-26 15:39:47 +0200

    feat: add exports for experimental components

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './components';
 export * from './essentials';
+export * as experimental from './experimental';
 export * from './hooks';
 export * from './icons';
 export * from './utils';
