6f5b1eb462a7cabffe3e37984a5a90c3f1269c19
Lena Rashkovan <lena.rashkovan@free-now.com>
2024-09-10 16:33:36 +0200

    feat(button): use color variables

--- src/components/experimental/Button/Button.tsx
+++ src/components/experimental/Button/Button.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { Button as BaseButton } from '../../Button/Button';
 import { theme as experimentalTheme } from '../../../essentials/experimental/theme';
+import { getSemanticValue, getSemanticHslValue } from '../../../essentials/experimental/cssVariables';
 import { get } from '../../../utils/experimental/themeGet';
 import { Text } from '../Text/Text';
 
@@ -10,10 +11,10 @@ const ButtonStyled = styled(BaseButton)<{ secondary: boolean }>`
     padding: ${experimentalTheme.space[4]}rem ${experimentalTheme.space[6]}rem;
     height: unset;
 
-    background-color: hsl(350, 92%, 25%); // var(--sys-color-accent, #790518);
+    background-color: ${getSemanticValue('accent')};
 
     &:hover {
-        background-color: hsl(350, 53%, 36%); // var(--sys-color-accent, #790518) * opacity 0.16
+        background-color: hsla(${getSemanticHslValue('accent')}, 0.84);
     }
 
     ${props =>
