01b463f1e90b4c8945ec47fad020a0d1c2ab9d77
Lena Rashkovan <lena.rashkovan@free-now.com>
2024-09-19 17:41:14 +0200

    feat(label): add Label

---
+++ src/components/experimental/Label/Label.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import { variant } from 'styled-system';
+import React, { ComponentPropsWithoutRef, ReactElement } from 'react';
+import { textStyles } from '../Text/Text';
+import { getSemanticValue } from '../../../essentials/experimental/cssVariables';
+import { get } from '../../../utils/experimental/themeGet';
+
+type LabelType = 'default' | 'positive' | 'negative';
+
+interface LabelProps extends ComponentPropsWithoutRef<'span'> {
+    /**
+     * Set the appropriate colors for the component with 'default' as a default
+     */
+    type?: LabelType;
+}
+
+const variantStyles = variant({
+    prop: '$type',
+    variants: {
+        default: {
+            color: getSemanticValue('on-surface'),
+            backgroundColor: getSemanticValue('surface-variant')
+        },
+        positive: {
+            color: getSemanticValue('on-positive-container'),
+            backgroundColor: getSemanticValue('positive-container')
+        },
+        negative: {
+            color: getSemanticValue('on-negative-container'),
+            backgroundColor: getSemanticValue('negative-container')
+        }
+    }
+});
+
+const StyledLabel = styled.span<{ $type: LabelType }>`
+    display: inline-flex;
+    box-sizing: border-box;
+    border-radius: ${get('radii.2')};
+    padding: ${get('space.1')} ${get('space.2')};
+
+    ${textStyles.variants.label2};
+
+    ${props => variantStyles(props)}
+`;
+
+function Label({ children, type = 'default', ...restProps }: LabelProps): ReactElement {
+    return (
+        <StyledLabel $type={type} {...restProps}>
+            {children}
+        </StyledLabel>
+    );
+}
+
+export { Label, LabelProps };
