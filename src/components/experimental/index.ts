7242087e643c5117de54ae0871cf4f48864dd68f
Lena Rashkovan <lena.rashkovan@free-now.com>
2024-09-23 13:01:52 +0200

    fix(experimental): export label

--- src/components/experimental/index.ts
+++ src/components/experimental/index.ts
@@ -1,4 +1,5 @@
-export { Chip } from './Chip/Chip';
-export { Text } from './Text/Text';
 export { Button } from './Button/Button';
+export { Chip } from './Chip/Chip';
+export { Label } from './Label/Label';
+export { Text } from './Text/Text';
 export { TextField } from './TextField/TextField';
