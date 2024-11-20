383a7439c5ca0787f9dc9724abb58b153ef8c70d
dornelasnelson <nelson.dornelas@free-now.com>
2024-11-20 15:32:13 +0100

    feat: add more car and location icons

--- src/icons/experimental/index.ts
+++ src/icons/experimental/index.ts
@@ -1,2 +1,5 @@
 export { default as CalendarTodayOutlineIcon } from './CalendarTodayOutlineIcon';
 export { default as AccountOutlineIcon } from "./AccountOutlineIcon";
+export { default as CarGroupOutlineIcon } from "./CarGroupOutlineIcon";
+export { default as CarOutlineIcon } from "./CarOutlineIcon";
+export { default as LocationIcon } from "./LocationIcon";
