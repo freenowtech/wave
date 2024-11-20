31e0c21184aefaaa779ce9742493316d8d862b37
Elena Rashkovan <mi.usu@yandex.ru>
2024-11-20 15:37:42 +0100

    feat(table): add experimental component (#493)
    * feat(table): add experimental component
    
    * feat(table): add skeleton
    
    * docs(table): add examples
    
    * fix(table): fix lost generic props
    
    * chore: update stylelint whitelist
    
    * style(table): move comment
    
    ---------
    
    Co-authored-by: Lena Rashkovan <lena.rashkovan@free-now.com>

---
+++ src/components/experimental/Table/Table.tsx
@@ -0,0 +1,96 @@
+import {
+    Table as BaseTable,
+    TableProps,
+    Cell as BaseCell,
+    Column as BaseColumn,
+    Row as BaseRow,
+    TableBody,
+    TableHeader
+} from 'react-aria-components';
+import styled from 'styled-components';
+import { get } from '../../../utils/experimental/themeGet';
+import { textStyles } from '../Text/Text';
+import { getSemanticValue } from '../../../essentials/experimental';
+
+const Table = styled(BaseTable)`
+    border-collapse: collapse;
+    border-spacing: 0;
+    position: relative;
+    width: 100%;
+    max-height: 100vh;
+    background: ${getSemanticValue('surface')};
+    color: ${getSemanticValue('on-surface')};
+` as typeof BaseTable;
+
+const Cell = styled(BaseCell)`
+    padding: 0 ${get('space.3')};
+    position: relative;
+
+    &::before {
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        content: '';
+        border-radius: inherit;
+        opacity: 0;
+        transition: opacity ease 200ms;
+    }
+
+    &:first-of-type {
+        border-radius: ${get('radii.4')} 0 0 ${get('radii.4')};
+    }
+
+    &:last-of-type {
+        border-radius: 0 ${get('radii.4')} ${get('radii.4')} 0;
+    }
+
+    &[data-focused] {
+        outline: 0;
+    }
+` as typeof BaseCell;
+
+/* Z-Index is needed for sticky header cells to be on top of other cells */
+const Column = styled(BaseColumn)`
+    position: sticky;
+    top: 0;
+    z-index: 1;
+    padding: 0 ${get('space.3')};
+    height: 3rem;
+    background: ${getSemanticValue('surface')};
+    border-bottom: 1px solid ${getSemanticValue('divider')};
+    text-align: start;
+    white-space: nowrap;
+    outline: 0;
+    ${textStyles.variants.title2}
+` as typeof BaseColumn;
+
+const Row = styled(BaseRow)`
+    height: 3rem;
+    border-bottom: 1px solid ${getSemanticValue('divider')};
+    border-radius: ${get('radii.4')};
+    ${textStyles.variants.body1}
+
+    &[data-hovered] td::before {
+        background: ${getSemanticValue('on-surface')};
+        opacity: 0.08;
+    }
+
+    &[data-selected] {
+        background: ${getSemanticValue('interactive-container')};
+    }
+
+    &[data-focused] {
+        outline: 0.125rem solid ${getSemanticValue('accent')};
+        outline-offset: -0.125rem;
+    }
+` as typeof BaseRow;
+
+const Skeleton = styled.div`
+    height: 1rem;
+    border-radius: ${get('radii.2')};
+    background: ${getSemanticValue('surface-variant')};
+`;
+
+export { Table, TableProps, Cell, Column, Row, TableBody, TableHeader, Skeleton };
