2ddc5e0bf00f7bfa2ecc812c367b5ac6e7fe24be
Lena Rashkovan <lena.rashkovan@free-now.com>
2024-12-04 11:42:18 +0100

    feat(icons): add experimental spinner icon

--- src/icons/experimental/index.ts
+++ src/icons/experimental/index.ts
@@ -1,5 +1,6 @@
 export { default as CalendarTodayOutlineIcon } from './CalendarTodayOutlineIcon';
-export { default as AccountOutlineIcon } from "./AccountOutlineIcon";
-export { default as CarGroupOutlineIcon } from "./CarGroupOutlineIcon";
-export { default as CarOutlineIcon } from "./CarOutlineIcon";
-export { default as LocationIcon } from "./LocationIcon";
+export { default as AccountOutlineIcon } from './AccountOutlineIcon';
+export { default as CarGroupOutlineIcon } from './CarGroupOutlineIcon';
+export { default as CarOutlineIcon } from './CarOutlineIcon';
+export { default as LocationIcon } from './LocationIcon';
+export { default as SpinnerIcon } from './SpinnerIcon';
