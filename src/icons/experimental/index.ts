802703e229d458c92145e981e286becabf5f8a46
dornelasnelson <nelson.dornelas@free-now.com>
2025-01-28 12:16:28 +0100

    feat: add PhoneCarOutlineIcon

--- src/icons/experimental/index.ts
+++ src/icons/experimental/index.ts
@@ -4,3 +4,4 @@ export { default as CarGroupOutlineIcon } from './CarGroupOutlineIcon';
 export { default as CarOutlineIcon } from './CarOutlineIcon';
 export { default as LocationIcon } from './LocationIcon';
 export { default as SpinnerIcon } from './SpinnerIcon';
+export { default as PhoneCarOutlineIcon } from './PhoneCarOutlineIcon';
