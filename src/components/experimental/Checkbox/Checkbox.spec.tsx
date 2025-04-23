b0c148e6bc00e1fc1cf699c2d92d65c59e0e6f9f
Alan Fernández Saavedra <alan.f@msn.com>
2025-04-23 17:24:50 +0200

    test: checkbox unit test

---
+++ src/components/experimental/Checkbox/Checkbox.spec.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import * as React from 'react';
+import { Checkbox } from './Checkbox';
+
+describe('Checkbox', () => {
+    it('selects/unselects the checkbox on clicking the label', async () => {
+        const user = userEvent.setup();
+        const labelText = 'Click Me!';
+        const onClickCallback = jest.fn();
+        const checkbox = render(<Checkbox label={labelText} onChange={e => onClickCallback(e.target.checked)} />);
+        const label = checkbox.getByText(labelText);
+
+        await user.click(label);
+        expect(onClickCallback).toBeCalledWith(true);
+
+        await user.click(label);
+        expect(onClickCallback).toBeCalledWith(false);
+    });
+});
