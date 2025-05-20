938f4eb621e9bef751fc8af115c0746751220177
lena.rashkovan <lena.rashkovan@free-now.com>
2025-05-20 18:47:42 +0200

    feat(radio-group): add experimental component

---
+++ src/components/experimental/RadioGroup/RadioGroup.styled.ts
@@ -0,0 +1,25 @@
+import {
+    FieldError,
+    RadioGroup as BaseRadioGroup,
+    RadioGroupProps as BaseRadioGroupProps,
+    ValidationResult,
+    Label
+} from 'react-aria-components';
+import styled from 'styled-components';
+
+import { themeGet } from '../../../utils/experimental';
+
+export const List = styled.div`
+    display: flex;
+    gap: ${themeGet('space.4')};
+`;
+
+export const RadioGroup = styled(BaseRadioGroup)`
+    &[data-orientation='horizontal'] ${List} {
+        flex-direction: row;
+    }
+
+    &[data-orientation='vertical'] ${List} {
+        flex-direction: column;
+    }
+`;
