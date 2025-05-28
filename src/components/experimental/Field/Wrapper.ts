3cfa5a5f68ec1e9c7ed5d14585fd27827d44b818
lena.rashkovan <lena.rashkovan@free-now.com>
2025-05-28 16:10:36 +0200

    fix(field): fix disabled state

--- src/components/experimental/Field/Wrapper.ts
+++ src/components/experimental/Field/Wrapper.ts
@@ -1,15 +1,6 @@
 import styled from 'styled-components';
 import { get } from '../../../utils/experimental/themeGet';
-import { FakeInput } from './FakeInput';
 
 export const Wrapper = styled.div`
     padding: ${get('space.2')} 0;
-
-    &:has(input[data-disabled]) {
-        opacity: 0.38;
-
-        ${FakeInput} {
-            pointer-events: none;
-        }
-    }
 `;
